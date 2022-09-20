95c4066394da79a669d3d21d41d175971932ce15
Luiz Pires <lpiresjunior@gmail.com>
2022-09-20 00:11:05 -0300

    challenge completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,12 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
-    const id = String(user_id);
+    try {
+      const { user_id } = request.headers;
+      const id = String(user_id);
 
-    const allUsers = this.listAllUsersUseCase.execute({ user_id: id });
+      const allUsers = this.listAllUsersUseCase.execute({ user_id: id });
 
-    return response.json(allUsers);
+      return response.json(allUsers);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
